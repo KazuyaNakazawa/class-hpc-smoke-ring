200851e3ad486c9d009c320d399a0ba5e9dac9f3
KazuyaNakazawa <1685006t@stu.kobe-u.ac.jp>
2019-01-24 13:55:59 +0900

    ompのテスト

---
+++ src/hello_omp.f90
@@ -0,0 +1,9 @@
+program hello_omp
+  use omp_lib
+  implicit none
+  print *, "----start"
+  !$omp parallel
+  print *, "thread_num =", omp_get_num_threads(), " I am ", omp_get_thread_num()
+  !$omp end parallel
+  print *, "----end"
+end program hello_omp
